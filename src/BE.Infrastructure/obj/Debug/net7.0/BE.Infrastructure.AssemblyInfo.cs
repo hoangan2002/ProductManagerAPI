70fb7140fe218d33098f4119aa3041d42edadf4b
hoangan2002 <trandinhhoangan2002@gmail.com>
2023-12-02 00:59:25 +0700

    Rename

--- src/BE.Infrastructure/obj/Debug/net7.0/BE.Infrastructure.AssemblyInfo.cs
+++ src/BE.Infrastructure/obj/Debug/net7.0/BE.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c743f6ce026f61d2874fdc5fa496891bce5af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
