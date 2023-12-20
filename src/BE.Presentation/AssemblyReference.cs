0d60927d8458945865127d8ad46f820bd086091e
Hoàng Ân <144087045+hoangan2002@users.noreply.github.com>
2023-12-20 07:29:06 +0700

    Delete src/BE.Presentation directory

--- src/BE.Presentation/AssemblyReference.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-namespace BE.Presentation;
-public static class AssemblyReference
-{
-    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
-}
